e710d05ccc2761f169d252a6b17be995242b6e3f
athishethi <athish4943@gmail.com>
2026-04-19 11:57:37 +0530

    Refactor restaurant name generator input method

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from langchain_helper import generate_restaurant_name_and_items
 
-st.title("Restaurant Name Generator")
+st.title("Restaurant Name Generator 🍽️")
 
-cuisine = st.sidebar.selectbox("pick a cuisine",("Indian","Italian","Mexican","Arabic"))
-
+cuisine = st.text_input("Enter cuisine type")
 
 if cuisine:
-    response = generate_restaurant_name_and_items(cuisine)
-    st.header(response['restaurant_name'].strip())
-    menu_items = response['menu_items'].strip().split(",")
-    st.write("Menu Items")
-    for item in menu_items:
-        st.write("-",item)
+    result = generate_restaurant_name_and_items(cuisine)
+    st.write(result)
